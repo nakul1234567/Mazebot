cdc6c323988ee3915b85f0a7d39462f6582f3534
Alankrit <alankrit019@gmail.com>
2026-03-10 19:49:40 -0400

    changed model.py from DQN to PPO

--- model.py
+++ model.py
@@ -1,24 +1,49 @@
-from stable_baselines3 import DQN
+from stable_baselines3 import PPO
 import gymnasium
 import coverage_gridworld
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments for training
-vec_env = make_vec_env("sneaky_enemies", n_envs=4)
+vec_env = make_vec_env("just_go", n_envs=4)
 
-model = DQN("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=100000)
-model.save("gridworld")
+model = PPO(
+    "MlpPolicy",
+    vec_env,
+    learning_rate=5e-4,
+    n_steps=1024,
+    batch_size=256,
+    n_epochs=10,
+    gamma=0.99,
+    gae_lambda=0.95,
+    clip_range=0.2,
+    ent_coef=0.01,
+    vf_coef=0.5,
+    max_grad_norm=0.5,
+    verbose=1
+)
+
+model.learn(total_timesteps=10000)
+model.save("ppo_gridworld")
 
 # Single environment for testing
-env = gymnasium.make("sneaky_enemies", render_mode="human", predefined_map_list=None, activate_game_status=True)
-model = DQN.load("gridworld")
+env = gymnasium.make(
+    "just_go",
+    render_mode="human",
+    predefined_map_list=None,
+    activate_game_status=True
+)
+
+model = PPO.load("ppo_gridworld")
+
 obs, _ = env.reset()
 terminated = False
+truncated = False
 total_reward = 0
-while not terminated:
-    action, _states = model.predict(obs)
-    obs, rewards, terminated, truncated, info = env.step(action)
-    total_reward += rewards
+
+while not (terminated or truncated):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, terminated, truncated, info = env.step(action)
+    total_reward += reward
+
 print(f"Total reward: {total_reward}")
 env.close()
